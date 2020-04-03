7e15cf7f2093081102beb6cfe671eb1176a2a4cf
Mongithabet <stechnopc@yahoo.fr>
2020-04-03 21:29:10 +0100

    main

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-$('.slider-one').slick();
+$('.slider-one')
+.not(".slick-initialized")
+.slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots:true,
+  prevArrow:".site-slider .slider-btn .prev",
+  nextArrow:".site-slider .slider-btn .next"
+
+});
